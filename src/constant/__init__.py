73f17a4b9b5dcef43a1f43ceabd52e769df22e85
WasimSamirArsad <wasimakhtar12091@gmail.com>
2024-09-19 14:05:00 +0530

    notebook EDA

---
+++ src/constant/__init__.py
@@ -0,0 +1,13 @@
+import os
+
+AWS_S3_BUCKET_NAME="wafer-fault"
+MONGO_DATABASE_NAME="pwskills"
+MONGO_COLLECTION_NAME="waferfault"
+
+TARGET_COLUMN="quality"
+MONGO_DB_URL=""
+
+MODEL_FILE_NAME="model"
+MODEL_FILE_EXTENSION=".pkl"
+
+
