cd6e35b926e59ec1a2eab086b7eb1f43e321b538
WasimSamirArsad <wasimakhtar12091@gmail.com>
2024-09-22 12:45:39 +0530

    wafer fault detection ml project

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,jsonify,request,send_file
+from flask import Flask,render_template,jsonify,request,send_file,redirect
 from src.exception import CustomException
 from src.logger import logging as lg
 import os
@@ -13,7 +13,7 @@ app=Flask(__name__)
 
 @app.route("/")
 def home():
-    return "welcome to my application"
+    return render_template('home.html')
 
 @app.route("/train")
 def train_route():
